ebeb05739f5e0d3a7d0bd3b969128e5f4958e641
GabrielDCelery <gabriel.d.celery@gmail.com>
2021-09-14 10:32:26 +0100

    moar code

---
+++ a_man_with_a_plan/run.sh
@@ -0,0 +1,1 @@
+cargo build && ./target/debug/puzzle_a_man_with_a_plan.exe < ./dev/input.txt
--- a_man_with_a_plan/src/main.rs
+++ a_man_with_a_plan/src/main.rs
@@ -8,7 +8,8 @@ macro_rules! parse_input {
 }
 
 #[derive(Debug, Copy, Clone)]
-enum TerrainType {
+enum Terrain {
+    None,
     Grassland,
     Water,
     Mountain,
@@ -17,22 +18,23 @@ enum TerrainType {
     PointOfInterest,
 }
 
-impl TerrainType {
-    fn convert_str_to_enum(string: &str) -> Result<TerrainType, ()> {
+impl Terrain {
+    fn convert_str_to_enum(string: &str) -> Result<Terrain, ()> {
         return match string {
-            "G" => Ok(TerrainType::Grassland),
-            "W" => Ok(TerrainType::Water),
-            "M" => Ok(TerrainType::Mountain),
-            "S" => Ok(TerrainType::Swamp),
-            "R" => Ok(TerrainType::Ravine),
-            "I" => Ok(TerrainType::PointOfInterest),
+            "G" => Ok(Terrain::Grassland),
+            "W" => Ok(Terrain::Water),
+            "M" => Ok(Terrain::Mountain),
+            "S" => Ok(Terrain::Swamp),
+            "R" => Ok(Terrain::Ravine),
+            "I" => Ok(Terrain::PointOfInterest),
             _ => Err(()),
         };
     }
 }
 
-#[derive(Debug, PartialEq, Eq, Hash)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
 enum PointOfInterest {
+    None,
     House,
     Castle,
     Blacksmith,
@@ -59,65 +61,184 @@ impl PointOfInterest {
     }
 }
 
+type Coordinates = [u8; 2];
+
 #[derive(Debug, Copy, Clone)]
-struct Terrain {
-    terrain_type: TerrainType,
+struct MapCell {
+    terrain: Terrain,
+    point_of_interest: PointOfInterest,
 }
 
-type TerrainMatrix = Vec<Vec<Terrain>>;
-type PointOfInterests = HashMap<PointOfInterest, [u8; 2]>;
+type MapMatrix = Vec<Vec<MapCell>>;
+type PointOfInterestMap = HashMap<PointOfInterest, Coordinates>;
 
 #[derive(Debug)]
 struct GameMap {
     width: usize,
     height: usize,
-    num_of_point_of_interests: usize,
-    terrain_matrix: TerrainMatrix,
-    point_of_interests: PointOfInterests,
+    map_matrix: MapMatrix,
+    point_of_interest_map: PointOfInterestMap,
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+enum Weapon {
+    None,
+    Sword,
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+enum Mount {
+    None,
+    Horse,
 }
 
 #[derive(Debug)]
-struct GameState<'gm> {
+struct Player {
+    weapon: Weapon,
+    mount: Mount,
     objective: PointOfInterest,
-    map: &'gm GameMap,
+    has_completed_objective: bool,
+    has_finished_the_game: bool,
+}
+
+impl Player {
+    fn clone(&self) -> Player {
+        return Player {
+            weapon: self.weapon,
+            mount: self.mount,
+            objective: self.objective,
+            has_completed_objective: self.has_completed_objective,
+            has_finished_the_game: self.has_finished_the_game,
+        };
+    }
+
+    fn can_do(&self, point_of_interest: &PointOfInterest) -> bool {
+        return match point_of_interest {
+            PointOfInterest::House => false,
+            PointOfInterest::Castle => {
+                self.has_finished_the_game == false && self.has_completed_objective == true
+            }
+            PointOfInterest::Princess => {
+                self.has_finished_the_game == false && self.has_completed_objective == false
+            }
+            PointOfInterest::Blacksmith => {
+                self.has_finished_the_game == false && self.weapon == Weapon::None
+            }
+            _ => false,
+        };
+    }
+
+    fn apply_point_of_interest_effect(&mut self, point_of_interest: &PointOfInterest) {
+        match point_of_interest {
+            PointOfInterest::Castle => self.has_finished_the_game = true,
+            PointOfInterest::Princess => {
+                if self.objective == PointOfInterest::Princess {
+                    self.has_completed_objective = true
+                }
+            }
+            PointOfInterest::Blacksmith => self.weapon = Weapon::Sword,
+            PointOfInterest::Stable => self.mount = Mount::Horse,
+            _ => (),
+        };
+    }
+
+    fn apply_terrain_effect(&mut self, terrain: &Terrain) {
+        match terrain {
+            Terrain::Water => self.weapon = Weapon::None,
+            _ => (),
+        };
+    }
+}
+
+#[derive(Debug)]
+struct PlayerMovesTreeNode {
+    player: Player,
+    point_of_interest: PointOfInterest,
+    children: Vec<PlayerMovesTreeNode>,
+}
+
+impl PlayerMovesTreeNode {
+    fn new(
+        player: &Player,
+        point_of_interest: &PointOfInterest,
+        available_point_of_interests: &Vec<PointOfInterest>,
+    ) -> PlayerMovesTreeNode {
+        let cloned_player = player.clone();
+        let mut children: Vec<PlayerMovesTreeNode> = vec![];
+
+        for chosen_point_of_interest in available_point_of_interests {
+            let mut cloned_player = player.clone();
+            if cloned_player.can_do(chosen_point_of_interest) {
+                let mut cloned_point_of_interests = available_point_of_interests.clone();
+                let index = available_point_of_interests
+                    .iter()
+                    .position(|&r| r == *chosen_point_of_interest)
+                    .unwrap();
+                cloned_point_of_interests.remove(index);
+                cloned_player.apply_point_of_interest_effect(chosen_point_of_interest);
+                children.push(PlayerMovesTreeNode::new(
+                    &cloned_player,
+                    &chosen_point_of_interest,
+                    &cloned_point_of_interests,
+                ))
+            }
+        }
+
+        return PlayerMovesTreeNode {
+            player: cloned_player,
+            point_of_interest: *point_of_interest,
+            children,
+        };
+    }
 }
 
 fn main() {
+    //##################### READ GAME INPUT - START
     let mut input_line = String::new();
     io::stdin().read_line(&mut input_line).unwrap();
     let game_inputs = input_line.split(" ").collect::<Vec<_>>();
-
     let map_width = parse_input!(game_inputs[0], usize);
     let map_height = parse_input!(game_inputs[1], usize);
     let num_of_point_of_interests = parse_input!(game_inputs[2], usize);
+    //##################### READ GAME INPUT - END
 
-    let mut terrain_matrix = vec![
+    let mut player = Player {
+        weapon: Weapon::None,
+        mount: Mount::None,
+        objective: PointOfInterest::None,
+        has_completed_objective: false,
+        has_finished_the_game: false,
+    };
+
+    let mut map_matrix = vec![
         vec![
-            Terrain {
-                terrain_type: TerrainType::Grassland
+            MapCell {
+                terrain: Terrain::None,
+                point_of_interest: PointOfInterest::None,
             };
             map_width
         ];
         map_height
     ];
-    let mut point_of_interests = HashMap::new();
 
+    let mut point_of_interest_map = HashMap::new();
+
+    //##################### READ GAME INPUT - START
     let mut input_line = String::new();
     io::stdin().read_line(&mut input_line).unwrap();
-    let objective_as_str = input_line.trim().to_string();
-    let objective = PointOfInterest::convert_str_to_enum(&objective_as_str).unwrap();
-
+    let point_of_interest_type_as_str = input_line.trim().to_string();
+    player.objective =
+        PointOfInterest::convert_str_to_enum(&point_of_interest_type_as_str).unwrap();
     for h in 0..map_height {
         let mut input_line = String::new();
         io::stdin().read_line(&mut input_line).unwrap();
         let line = input_line.trim_matches('\n').to_string();
         for w in 0..map_width {
             let terrain_type_as_str = line.chars().nth(w).unwrap().to_string();
-            let terrain_type = TerrainType::convert_str_to_enum(&terrain_type_as_str).unwrap();
-            terrain_matrix[h][w] = Terrain { terrain_type }
+            let terrain = Terrain::convert_str_to_enum(&terrain_type_as_str).unwrap();
+            map_matrix[h][w].terrain = terrain
         }
     }
-
     for _ in 0..num_of_point_of_interests as usize {
         let mut input_line = String::new();
         io::stdin().read_line(&mut input_line).unwrap();
@@ -125,25 +246,28 @@ fn main() {
         let point_of_interest = game_inputs[0].trim().to_string();
         let x = parse_input!(game_inputs[1], u8);
         let y = parse_input!(game_inputs[2], u8);
-
-        point_of_interests.insert(
+        point_of_interest_map.insert(
             PointOfInterest::convert_str_to_enum(&point_of_interest).unwrap(),
             [y, x],
         );
     }
+    //##################### READ GAME INPUT - END
+
+    let available_point_of_interests: Vec<PointOfInterest> =
+        point_of_interest_map.keys().cloned().collect();
 
     let game_map = GameMap {
         width: map_width,
         height: map_height,
-        num_of_point_of_interests,
-        terrain_matrix,
-        point_of_interests,
+        map_matrix,
+        point_of_interest_map,
     };
 
-    let game_state = GameState {
-        objective,
-        map: &game_map,
-    };
+    let root_node = PlayerMovesTreeNode::new(
+        &player,
+        &PointOfInterest::House,
+        &available_point_of_interests,
+    );
 
-    println!("{:?}", game_state)
+    println!("{:?}", root_node);
 }
