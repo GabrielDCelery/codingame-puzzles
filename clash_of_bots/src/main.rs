35a62f67a4ab528e5cd921268926296cffface84
GabrielDCelery <gabriel.d.celery@gmail.com>
2022-02-23 18:08:44 +0000

    added first iteration of command wrappers to clash of bots

--- clash_of_bots/src/main.rs
+++ clash_of_bots/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::io;
 use std::slice::Iter;
 
@@ -21,6 +22,10 @@ impl Coordinates {
     fn add_vector(&mut self, vector: &Vector) {
         self.x += vector.x;
         self.y += vector.y;
+    }
+
+    fn get_manhattan_distance(&self, other: &Coordinates) -> i8 {
+        return (other.x - self.x).abs() + (other.y - self.y).abs();
     }
 }
 
@@ -81,51 +86,47 @@ impl Direction {
     }
 }
 
-trait Criterion {
-    fn new() -> Self;
-    fn is_true(&self, local_area: &LocalArea) -> bool;
-}
-
-struct HasEnemyNextToMe {}
-
-impl Criterion for HasEnemyNextToMe {
-    fn new() -> HasEnemyNextToMe {
-        return HasEnemyNextToMe {};
-    }
-
-    fn is_true(&self, local_area: &LocalArea) -> bool {
-        for direction in Direction::iterator() {
-            let vector = direction.to_vector();
-            let mut coordinates = Coordinates::new(2, 2);
-            coordinates.add_vector(&vector);
-            if local_area.is_enemy_at_coords(&coordinates) {
-                return true;
-            };
-        }
-        return false;
-    }
+#[derive(Debug)]
+struct Robot {
+    id: usize,
+    is_friendly: bool,
+    health: i8,
+    coordinates: Coordinates,
 }
 
 #[derive(Debug)]
 struct LocalArea {
     id: usize,
     grid: [[i8; 5]; 5],
+    robots: Vec<Robot>,
 }
 
 impl LocalArea {
-    fn new() -> LocalArea {
+    fn new(id: usize) -> LocalArea {
         return LocalArea {
-            id: 0,
+            id,
             grid: [[0; 5]; 5],
+            robots: vec![],
         };
-    }
-
-    fn set_id(&mut self, id: usize) {
-        self.id = id;
     }
 
     fn set_local_area_cell(&mut self, coordinates: &Coordinates, value: i8) {
         self.grid[coordinates.y as usize][coordinates.x as usize] = value;
+        if value == 0 {
+            return;
+        }
+        let id = self.robots.len();
+        let is_friendly = value > 0;
+        let health = value.abs();
+        self.robots.insert(
+            id,
+            Robot {
+                id,
+                is_friendly,
+                health,
+                coordinates: coordinates.clone(),
+            },
+        )
     }
 
     fn get_robot_health_at_coords(&self, coordinates: &Coordinates) -> i8 {
@@ -137,11 +138,147 @@ impl LocalArea {
     }
 }
 
+#[derive(Debug, PartialEq, Eq, Hash)]
+enum ConditionName {
+    DefaultTrue,
+    AmIAlone,
+}
+
+trait Condition {
+    fn is_true(&self, local_area: &LocalArea) -> bool;
+}
+
+type ConditionDictionary = HashMap<ConditionName, Box<dyn Condition>>;
+
+struct DefaultTrue {}
+
+impl Condition for DefaultTrue {
+    fn is_true(&self, _: &LocalArea) -> bool {
+        return true;
+    }
+}
+
+struct AmIAlone {}
+
+impl Condition for AmIAlone {
+    fn is_true(&self, local_area: &LocalArea) -> bool {
+        let friendly: Vec<&Robot> = local_area
+            .robots
+            .iter()
+            .filter(|v| v.is_friendly == true)
+            .collect();
+        return friendly.len() == 1;
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Hash, Copy, Clone)]
+enum ActionName {
+    SeekFriends,
+    GuardMySelf,
+}
+
+trait Action {
+    fn get_command(&self, local_area: &LocalArea) -> String;
+}
+
+type ActionDictionary = HashMap<ActionName, Box<dyn Action>>;
+
+struct SeekFriends {}
+
+impl Action for SeekFriends {
+    fn get_command(&self, _: &LocalArea) -> String {
+        return "MOVE UP".to_string();
+    }
+}
+
+struct GuardMySelf {}
+
+impl Action for GuardMySelf {
+    fn get_command(&self, _: &LocalArea) -> String {
+        return "GUARD".to_string();
+    }
+}
+
+struct ActionConfiguration {
+    conditions: Vec<ConditionName>,
+    action_name: ActionName,
+}
+
+impl ActionConfiguration {
+    fn new(action_name: ActionName, conditions: Vec<ConditionName>) -> ActionConfiguration {
+        return ActionConfiguration {
+            action_name,
+            conditions,
+        };
+    }
+
+    fn is_matching_preconditions(
+        &self,
+        local_area: &LocalArea,
+        condition_dictionary: &ConditionDictionary,
+    ) -> bool {
+        for precondition in self.conditions.iter() {
+            let precondition_trait = condition_dictionary.get(&precondition).unwrap();
+            let is_true = precondition_trait.is_true(&local_area);
+            if is_true == false {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+struct GameAI<'a> {
+    condition_dictionary: &'a ConditionDictionary,
+    action_configurations: &'a Vec<ActionConfiguration>,
+    action_dictionary: &'a ActionDictionary,
+}
+
+impl GameAI<'_> {
+    fn pick_action(&self, local_area: &LocalArea) -> ActionName {
+        for action_configuration in self.action_configurations.iter() {
+            let is_matching = action_configuration
+                .is_matching_preconditions(&local_area, self.condition_dictionary);
+            if is_matching {
+                return action_configuration.action_name;
+            }
+        }
+        panic!("Could not find action matching conditions")
+    }
+
+    fn get_command_for_robot(&self, local_area: &LocalArea) -> String {
+        let action_name = self.pick_action(&local_area);
+        let action = self.action_dictionary.get(&action_name).unwrap();
+        return action.get_command(&local_area);
+    }
+}
+
 /**
  * Auto-generated code below aims at helping you parse
  * the standard input according to the problem statement.
  **/
 fn main() {
+    let mut condition_dictionary: ConditionDictionary = HashMap::new();
+
+    condition_dictionary.insert(ConditionName::AmIAlone, Box::new(AmIAlone {}));
+    condition_dictionary.insert(ConditionName::DefaultTrue, Box::new(DefaultTrue {}));
+
+    let mut action_dictionary: ActionDictionary = HashMap::new();
+
+    action_dictionary.insert(ActionName::SeekFriends, Box::new(SeekFriends {}));
+    action_dictionary.insert(ActionName::GuardMySelf, Box::new(GuardMySelf {}));
+
+    let action_configurations: Vec<ActionConfiguration> = vec![
+        ActionConfiguration::new(ActionName::SeekFriends, vec![ConditionName::AmIAlone]),
+        ActionConfiguration::new(ActionName::GuardMySelf, vec![ConditionName::DefaultTrue]),
+    ];
+
+    let game_ai = GameAI {
+        condition_dictionary: &condition_dictionary,
+        action_dictionary: &action_dictionary,
+        action_configurations: &action_configurations,
+    };
+
     // game loop
     loop {
         let mut input_line = String::new();
@@ -151,8 +288,7 @@ fn main() {
         let mut local_area_list: Vec<LocalArea> = vec![];
 
         for i in 0..number_of_robots as usize {
-            let mut local_area = LocalArea::new();
-            local_area.set_id(i);
+            let mut local_area = LocalArea::new(i);
 
             for j in 0..5 as usize {
                 let mut inputs = String::new();
@@ -160,27 +296,23 @@ fn main() {
                 let y = j as i8;
                 let mut x = 0 as i8;
                 for k in inputs.split_whitespace() {
-                    let cell = parse_input!(k, i8);
-                    local_area.set_local_area_cell(&Coordinates::new(x, y), cell);
+                    let value = parse_input!(k, i8);
+                    local_area.set_local_area_cell(&Coordinates::new(x, y), value);
                     x += 1;
                 }
             }
             local_area_list.insert(local_area.id, local_area);
         }
 
-        eprintln!("{:?}", local_area_list);
+        // Write an action using println!("message...");
+        // To debug: eprintln!("Debug message...");
+        // GUARD | MOVE (LEFT/RIGHT/UP/DOWN) | ATTACK (LEFT/RIGHT/UP/DOWN) | SELFDESTRUCTION <message>
 
         for i in 0..number_of_robots as usize {
             let local_area = local_area_list.get(i).unwrap();
+            let command = game_ai.get_command_for_robot(&local_area);
 
-            let criterion = HasEnemyNextToMe::new();
-
-            criterion.is_true(&local_area);
-            // Write an action using println!("message...");
-            // To debug: eprintln!("Debug message...");
-
-            // GUARD | MOVE (LEFT/RIGHT/UP/DOWN) | ATTACK (LEFT/RIGHT/UP/DOWN) | SELFDESTRUCTION <message>
-            println!("SELFDESTRUCTION");
+            println!("{}", command);
         }
     }
 }
